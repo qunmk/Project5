2ad8e7fef81d685993d4834e87b6edc2011b6a76
qunmk <94858392+qunmk@users.noreply.github.com>
2021-11-24 20:56:40 -0500

    Update Five.java
    Added one more value

--- src/project/five/Five.java
+++ src/project/five/Five.java
@@ -5,8 +5,11 @@ public class Five {
 	public static void main(String[] args) {
 	 int a=10;
 	 int b= 5;
-	 System.out.println(a+b);
+	 int c=2;
+	 System.out.println(a+b+c);
 	}
+	
+	
 	
 	 public static void div(int a, int b) {
 	
